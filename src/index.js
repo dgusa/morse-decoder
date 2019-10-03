cda64e9150cc616d31a3990bbb5a0c21dd070200
dgusa <dgus@tut.by>
2019-10-03 23:43:21 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,25 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let mas=[];
+    let j=0;
+        for (let i=0; i<=(expr.length-1)/10; i++) {
+            let litera =expr.substring(j, (j+10));
+            j=j+10;
+            while (litera[0] === '0') {
+                litera = litera.substr(1);
+            }
+            let literamorze=litera.replace(/10/gi, ".");
+            literamorze=literamorze.replace(/11/gi, "-");
+            if (literamorze === '**********') { 
+                literamorze=" ";
+            } else for (let morze in MORSE_TABLE)
+                if (morze===literamorze ) {
+                    literamorze=(MORSE_TABLE[morze]);
+                } 
+                mas.push(literamorze);
+        } 
+        return mas.join("")
 }
 
 module.exports = {
